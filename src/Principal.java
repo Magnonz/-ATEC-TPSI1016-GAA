90a98237848796368ef45ab6bffdbc2d46ac76e1
UserPL135Pc10 <UserPL135Pc10@SLFRPL135PC10.ensino.atec.local>
2016-10-26 11:36:22 +0100

    ""

--- src/Principal.java
+++ src/Principal.java
@@ -9,6 +9,22 @@ public class Principal {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hoje tenho sede!!!");
+		
+		int quotas = 300, recBaile = 200, recBar =250;
+		
+		int totalReceitas=quotas+recBaile+recBar;
+		
+		System.out.println("Então o seu total de receitas é: "+totalReceitas+"€");
+		System.out.println("São 5 euros por saber a sua despesa :D");
+		
+		int agua=100, luz=60, telefone=70;
+		
+		int bolacha=agua+luz+telefone;
+		
+		int totaldacena=totalReceitas-bolacha;
+		
+		System.out.println(totaldacena);
+		System.out.println("E são mais 5 euros :D");
 	}
 
 }
